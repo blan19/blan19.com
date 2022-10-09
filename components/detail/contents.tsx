4a5a303a65093d2627d922a9c4aba019b10189be
Junseo <oponize@naver.com>
2022-10-09 21:57:50 +0900

    feat: add mdx renderer

--- components/detail/contents.tsx
+++ components/detail/contents.tsx
@@ -1,7 +1,8 @@
 import ReactMarkdown from "react-markdown";
 import styled from "styled-components";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { a11yDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import remarkGfm from "remark-gfm";
 
 interface Props {
   markdown: string;
@@ -9,13 +10,16 @@ interface Props {
 
 const Content = ({ markdown }: Props) => {
   return (
-    <ReactMarkdown
+    <MarkDownRenderer
+      remarkPlugins={[remarkGfm]}
       components={{
         code: ({ node, inline, className, children, ...props }) => {
           const match = /language-(\w+)/.exec(className || "");
-          return !inline && match ? (
+          return inline ? (
+            <Highlight {...props}>{children}</Highlight>
+          ) : match ? (
             <SyntaxHighlighter
-              style={dark}
+              style={a11yDark as {}}
               language={match[1]}
               PreTag="div"
               {...props}
@@ -23,33 +27,39 @@ const Content = ({ markdown }: Props) => {
               {String(children).replace(/\n$/, "")}
             </SyntaxHighlighter>
           ) : (
-            <code className={className} {...props}>
-              {children}
-            </code>
+            <SyntaxHighlighter
+              style={a11yDark as {}}
+              language="textile"
+              PreTag="div"
+              {...props}
+            >
+              {String(children).replace(/\n$/, "")}
+            </SyntaxHighlighter>
           );
         },
       }}
     >
       {markdown}
-    </ReactMarkdown>
+    </MarkDownRenderer>
   );
 };
 
 export default Content;
 
-const MdxRenderer = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  margin: 0 auto;
-  word-break: break-all;
-  line-height: 1.8;
-  font-size: 1.15rem;
-  font-weight: 400;
-  p {
-    padding: 0.3rem 0;
-    color: ${(props) => props.theme.colors.font};
-  }
+const Highlight = styled.code`
+  background-color: ${({ theme }) => theme.colors.gray_2};
+  color: ${({ theme }) => theme.colors.primary};
+  font-weight: ${({ theme }) => theme.fontWeight.regular};
+  font-size: ${({ theme }) => theme.fontSize.body2r_pc};
+  padding: 0.25rem;
+  border-radius: 5px;
+`;
+
+const MarkDownRenderer = styled(ReactMarkdown)`
+  line-height: 1.75rem;
+  font-weight: ${({ theme }) => theme.fontWeight.regular};
+  font-size: ${({ theme }) => theme.fontSize.body2r_pc};
+
   h1,
   h2,
   h3 {
@@ -60,7 +70,7 @@ const MdxRenderer = styled.div`
   * + h1,
   * + h2,
   * + h3 {
-    margin-top: 8rem;
+    margin-top: 3rem;
   }
   hr + h1,
   hr + h2,
@@ -68,53 +78,50 @@ const MdxRenderer = styled.div`
     margin-top: 0;
   }
   h1 {
-    font-size: 2.5rem;
+    font-size: ${({ theme }) => theme.fontSize.title_mobile};
   }
   h2 {
-    font-size: 2rem;
+    font-size: ${({ theme }) => theme.fontSize.subtitle1_pc};
   }
   h3 {
-    font-size: 1.5rem;
+    font-size: ${({ theme }) => theme.fontSize.subtitle2r_pc};
   }
-  // Adjust Quotation Element Style
-  blockquote {
-    margin: 30px 0;
-    padding: 5px 15px;
-    border-left: 2px solid #8c5be9;
-    font-weight: 600;
+
+  p {
+    padding: 0.3rem 0;
+    color: ${(props) => props.theme.colors.font};
   }
-  // Adjust List Element Style
+
   ol,
   ul {
-    padding: 3rem 0;
+    padding: 1.5rem 0;
     list-style: none;
   }
   li::before {
     content: "# ";
     color: ${(props) => props.theme.colors.primary};
   }
+
+  blockquote {
+    margin: 30px 0;
+    padding: 5px 15px;
+    border-left: 2px solid ${({ theme }) => theme.colors.primary};
+    font-weight: ${({ theme }) => theme.fontWeight.bold};
+    background-color: ${({ theme }) => theme.colors.gray_2};
+  }
+
   hr {
     border: 1px solid ${(props) => props.theme.colors.sub};
-    margin: 10rem 0;
+    margin: 2.5rem 0;
   }
+
   a {
-    color: #8c5be9;
+    color: ${({ theme }) => theme.colors.primary};
     text-decoration: underline;
   }
-  pre[class*="language-"] {
-    margin: 30px 0;
-    padding: 1.5rem;
-    font-size: 1.25rem;
-    ::-webkit-scrollbar-thumb {
-      background: rgba(255, 255, 255, 0.5);
-      border-radius: 3px;
-    }
-  }
-  code[class*="language-"],
-  pre[class*="language-"] {
-    tab-size: 2;
-  }
+
   img {
     width: 100%;
+    padding: 3rem 0;
   }
 `;
