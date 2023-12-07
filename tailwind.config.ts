c1735ecdb69ef998cdbd10c533b2e3c869a3ec6e
Junseo Park <junseo_park@tmax.co.kr>
2023-12-07 22:28:23 +0900

    feat: extend custom color on tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,38 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      greyscale: {
+        0: "#f8f9fa",
+        1: "#f1f3f5",
+        2: "#e9ecef",
+        3: "#dee2e6",
+        4: "#ced4da",
+        5: "#adb5bd",
+        6: "#868e96",
+        7: "#495057",
+        8: "#343a40",
+        9: "#212529",
+      },
+      system: {
+        1: "#c92a2a",
+        2: "#e03131",
+      },
+    },
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
