44ac9702b9203fcc4fa38e254f18f8f3a72e8c55
Junseo <66871265+blan19@users.noreply.github.com>
2023-12-11 14:02:48 +0900

    style: update header logo and padding-y (#9)

--- src/components/layouts/header/index.tsx
+++ src/components/layouts/header/index.tsx
@@ -1,16 +1,15 @@
-import Image from "next/image";
 import Link from "next/link";
 import Navbar from "@/components/ui/navbar";
-import logo from "../../../../public/image/memoji-min.png";
 
 const Header = () => {
   return (
     <header className="sticky w-full z-[999] bg-greyscale-0 dark:bg-greyscale-9">
-      <div className="responsive flex justify-between">
+      <div className="responsive flex justify-between py-2">
         <Link href="/">
           <div className="flex items-center">
-            <Image placeholder="blur" alt="logo" src={logo} />
-            <h2 className="text-xl font-medium tracking-tighter">blan19</h2>
+            <h2 className="text-xl font-medium tracking-tighter italic">
+              blan19.com
+            </h2>
           </div>
         </Link>
         <div className="flex items-center gap-1">
