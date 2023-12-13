b679dd8b1ab9660d7b4d78912070f4763e198437
Junseo Park <junseo_park@tmax.co.kr>
2023-12-13 17:04:13 +0900

    feat: add best and latest blog posts query

--- src/app/db/queries.ts
+++ src/app/db/queries.ts
@@ -2,6 +2,7 @@
 
 import prisma from "@/lib/prisma";
 import { unstable_noStore as noStore } from "next/cache";
+import { unstable_cache as cache } from "next/cache";
 
 const getViewCount = async (slug: string) => {
   if (!process.env.POSTGRES_URL) {
@@ -19,4 +20,38 @@ const getViewCount = async (slug: string) => {
   return view;
 };
 
-export { getViewCount };
+const getBestBlogPosts = cache(
+  async () => {
+    const posts = await prisma.view.findMany({
+      orderBy: {
+        count: "asc",
+      },
+      take: 3,
+    });
+
+    return posts;
+  },
+  ["blan19-best-posts"],
+  {
+    revalidate: 3600,
+  }
+);
+
+const getLatestBlogPosts = cache(
+  async () => {
+    const posts = await prisma.view.findMany({
+      orderBy: {
+        createAt: "asc",
+      },
+      take: 3,
+    });
+
+    return posts;
+  },
+  ["blan19-latest-posts"],
+  {
+    revalidate: 3600,
+  }
+);
+
+export { getViewCount, getBestBlogPosts, getLatestBlogPosts };
